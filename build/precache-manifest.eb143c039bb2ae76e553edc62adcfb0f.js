ca7c34bcc3c0b1763fb5b4442f6ad07e3bd1e86d
Rabiekasem <54838133+Rabiekasem@users.noreply.github.com>
2019-12-02 16:20:44 +0100

    02.12

---
+++ build/precache-manifest.eb143c039bb2ae76e553edc62adcfb0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b271c49a43681f851120a603addca16b",
+    "url": "/tema-oppgave-4/index.html"
+  },
+  {
+    "revision": "62fd8194ff6a9e7bf71a",
+    "url": "/tema-oppgave-4/static/css/main.45b70914.chunk.css"
+  },
+  {
+    "revision": "c1ad1c427b0a8bcfab13",
+    "url": "/tema-oppgave-4/static/js/2.0840e82c.chunk.js"
+  },
+  {
+    "revision": "62fd8194ff6a9e7bf71a",
+    "url": "/tema-oppgave-4/static/js/main.717deb76.chunk.js"
+  },
+  {
+    "revision": "d1e6ee5819cf22026713",
+    "url": "/tema-oppgave-4/static/js/runtime-main.82651243.js"
+  }
+]);
