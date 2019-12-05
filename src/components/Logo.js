aae5b34e3d079f822eec0707052ee6cae0af36a8
Rabiekasem <54838133+Rabiekasem@users.noreply.github.com>
2019-12-05 16:05:12 +0100

    .

---
+++ src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+const Logo = () => {
+    return(
+        <main className="main1">
+           <img src="./components/image/logo.png" />
+        </main>
+    )
+}
+
+export default Logo
